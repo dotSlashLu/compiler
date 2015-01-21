8ce6cb57281716cc67d6ac94d104da34a0b8b178
dotslash.lu <dotslash.lu@gmail.com>
2015-01-21 17:39:11 +0800

    implement a simple predictive parser

--- predictive-parser.c
+++ predictive-parser.c
@@ -17,21 +17,22 @@
 *       for simplicity
 */
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_TOKEN_SIZE 20
+#define MAX_TOKEN_LENGTH 50
 
-enum tokens {
-        tok_expr,
-        tok_if,
-        tok_for,
-        tok_other = 0
-};
-static char lookahead[MAX_TOKEN_SIZE];
-static int  lookaheadTokType;
+static char lookahead[MAX_TOKEN_LENGTH];
 static FILE *file;
+
+void gettok();
+void stmt();
+void optexpr();
+void match(const char terminal[]);
+void syntaxerr();
 
 int main(int argc, char **argv)
 {
@@ -39,32 +40,108 @@ int main(int argc, char **argv)
                 fprintf(stderr, "Usage: %s filename\n", *argv);
                 exit(1);
         }
-        file = fopen(*++argv);
+
+        file = fopen(*++argv, "r");
+        if (file == NULL) {
+                perror("fopen: ");
+                exit(1);
+        }
+
+        gettok();
+        // only stmt is implemented for now
+        while (1) stmt();
+
+        fclose(file);
         return 0;
 }
 
 void stmt()
 {
-        switch (lookaheadTokType) {
-        case tok_expr:
-                match("expr"); match(';');
-                break;
-        case tok_if:
-                match(tok_if);
-                match('('); match(tok_expr);
-                match(')'); stmt();
-                break;
+        if (strcmp(lookahead, "expr") == 0) {
+                match("expr");
+                match(";");
         }
+
+        else if (strcmp(lookahead, "if") == 0) {
+                match("if"); match("("); match("expr"); match(")");
+                stmt();
+        }
+
+        else if (strcmp(lookahead, "for") == 0) {
+                match("for"); match("(");
+                optexpr(); match(";");
+                optexpr(); match(";");
+                optexpr(); match(")");
+                stmt();
+        }
+
+        else if (strcmp(lookahead, "other") == 0) match("other");
+
+        else syntaxerr();
 }
 
-void match(unsigned char terminal)
+void optexpr()
 {
-        if ()
+        if (strcmp(lookahead, "expr") == 0) match("expr");
+}
+
+void match(const char terminal[])
+{
+        printf("matching tok '%s' against '%s'\n", lookahead, terminal);
+        if (strcmp(lookahead, terminal) == 0) return gettok();
+        syntaxerr();
 }
 
 void gettok()
 {
-        while(isspace)
-                getc(file);
+#define unpeek()(fseek(file, -1L, SEEK_CUR))
+        lookahead[0] = '\0';
+        int i = 0;
+        int tok;
+
+        // skip spaces
+        while (isspace(tok = getc(file)));
+
+        // EOF
+        if (tok == EOF) {
+                printf("Token stream ended.\n");
+                exit(1);
+        }
+
+        lookahead[i++] = tok;
+        // only allow single punct for now
+        if (ispunct(tok)) {
+                lookahead[i++] = '\0';
+                return;
+        }
+
+        // peek
+        while ((tok = getc(file))) {
+                if (isalpha(tok)) {
+                        lookahead[i++] = tok;
+                        while (isalpha(tok = getc(file)))
+                                lookahead[i++] = tok;
+                        unpeek();
+                        break;
+                }
+
+                else if (ispunct(tok)) {
+                        lookahead[i++] = tok;
+                        // while (ispunct(tok = getc(file)))
+                        //         lookahead[i++] = tok;
+                        // unpeek();
+                        break;
+                }
+
+                else if (isspace(tok)) break;
+
+                else syntaxerr();
+        }
+        lookahead[i++] = '\0';
 }
 
+void syntaxerr()
+{
+       fprintf(stderr, "Syntax error at: %s\n", lookahead);
+       exit(1);
+}
